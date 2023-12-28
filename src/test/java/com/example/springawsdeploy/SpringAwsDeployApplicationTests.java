a5a8d2dae63622e89c014f0bc2b3017f0b0423b1
Dawid Lacheta <caushered@gmail.com>
2023-12-28 18:34:42 +0100

    init

---
+++ src/test/java/com/example/springawsdeploy/SpringAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
